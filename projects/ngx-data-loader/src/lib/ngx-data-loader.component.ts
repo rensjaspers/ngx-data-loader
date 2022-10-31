e2f0e3db3428a584f00e041facbc2d20fef08f43
Rens Jaspers <rens.jaspers@gmail.com>
2022-10-31 08:20:36 +0100

    refactor: rename parameter

--- projects/ngx-data-loader/src/lib/ngx-data-loader.component.ts
+++ projects/ngx-data-loader/src/lib/ngx-data-loader.component.ts
@@ -64,9 +64,9 @@ export class NgxDataLoaderComponent<T = unknown> implements OnInit, OnChanges {
     this.initialState = this.getInitialState();
 
     this.loadingState$ = this.getLoadingStateChanges().pipe(
-      scan((state, changes) => ({
+      scan((state, change) => ({
         ...state,
-        ...changes,
+        ...change,
       })),
       tap((state) => this.loadingStateChange.emit(state))
     );
