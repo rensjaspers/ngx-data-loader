a1dff108c0b86f6f8f3bd479e6deb813580dd52d
Rens Jaspers <rens.jaspers@gmail.com>
2022-11-03 16:27:14 +0100

    chore: clean up

--- projects/ngx-data-loader/src/lib/ng-changes.type.ts
+++
@@ -1,8 +0,0 @@
-export type NgChanges<Component, Props = ExcludeFunctions<Component>> = {
-  [Key in keyof Props]: {
-    previousValue: Props[Key];
-    currentValue: Props[Key];
-    firstChange: boolean;
-    isFirstChange(): boolean;
-  };
-};
