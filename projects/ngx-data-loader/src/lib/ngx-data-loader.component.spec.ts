3cc6d2ab4cabf1644535b3529205639c989ed7df
Rens Jaspers <rens.jaspers@gmail.com>
2023-05-20 17:43:53 +0200

    refactor: organize imports

--- projects/ngx-data-loader/src/lib/ngx-data-loader.component.spec.ts
+++ projects/ngx-data-loader/src/lib/ngx-data-loader.component.spec.ts
@@ -2,13 +2,12 @@ import {
   ComponentFixture,
   discardPeriodicTasks,
   fakeAsync,
-  flush,
   TestBed,
   tick,
 } from '@angular/core/testing';
 import { of, throwError, timer } from 'rxjs';
-import { LoadedComponent } from './loaded/loaded.component';
 import { ErrorComponent } from './error/error.component';
+import { LoadedComponent } from './loaded/loaded.component';
 import { LoadingStateTemplatePipe } from './loading-state-template.pipe';
 import { LoadingComponent } from './loading/loading.component';
 import { NgxDataLoaderComponent } from './ngx-data-loader.component';
